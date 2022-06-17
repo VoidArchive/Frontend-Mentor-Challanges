72b0ce01e54279baf40d3df27f758628b2971486
Anish Shrestha <anish.shrestha@smarten.com.np>
2022-06-17 10:33:09 +0545

    004 added

---
+++ 004/src/lib/Company.svelte
@@ -0,0 +1,53 @@
+<script>
+  import arrowDown from "../assets/icon-arrow-down.svg";
+  import arrowUp from "../assets/icon-arrow-up.svg";
+
+  let toggle = false;
+</script>
+
+<button on:click={() => (toggle = !toggle)}>
+  Company
+  {#if toggle}
+    <img class="arrow" src={arrowUp} alt="arrow-Up" />
+    <div class="content">
+      <a href="">History</a>
+      <a href="">Our Team</a>
+      <a href="">Blog</a>
+    </div>
+  {:else}
+    <img class="arrow" src={arrowDown} alt="arrow-Down" />
+  {/if}
+</button>
+
+<style>
+  .content {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 2rem 2rem;
+    align-items: start;
+    background-color: white;
+    margin-top: 1rem;
+    border-radius: 1rem;
+    box-shadow: 1px 1px 1rem hsl(0, 0%, 77%);
+  }
+
+  @media screen and (max-width: 900px) {
+    .content {
+      position: relative;
+      background-color: var(--white);
+      box-shadow: none;
+      border-radius: none;
+      margin: 0;
+    }
+  }
+  .arrow {
+    margin-left: 0.5rem;
+  }
+
+  button {
+    /* width: fit-content; */
+    padding: 0;
+  }
+</style>
---
+++ 004/src/lib/Sidebar.svelte
@@ -0,0 +1,76 @@
+<script>
+  import close from "../assets/icon-close-menu.svg";
+  import Features from "./Features.svelte";
+  import Company from "./Company.svelte";
+  export let open;
+</script>
+
+<aside class:open>
+  <button class="close-btn" on:click={() => (open = !open)}>
+    <img src={close} alt="Close-menu" />
+  </button>
+  <nav>
+    <div class="nav-left">
+      <Features />
+      <Company />
+      <a href="##">Careers</a>
+      <a href="##">About</a>
+    </div>
+
+    <div class="nav-right">
+      <button>Login</button>
+      <button class="btn-register">Register</button>
+    </div>
+  </nav>
+</aside>
+
+<style>
+  aside {
+    position: fixed;
+    top: 0;
+    width: 60%;
+    height: 100vh;
+    background-color: var(--white);
+    right: -100%;
+    transition: all 0.5s ease-in-out;
+  }
+
+  .open {
+    right: 0;
+  }
+
+  .close-btn {
+    position: absolute;
+    top: 2rem;
+    right: 2rem;
+  }
+
+  .nav-left {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1.5rem;
+    margin: 5rem 2rem;
+  }
+
+  .nav-right {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+  }
+
+  .btn-register {
+    border: 1px solid var(--gray);
+    width: 80%;
+    border-radius: 1rem;
+    padding: 0.8rem;
+  }
+
+  @media screen and (max-width: 400px) {
+    nav {
+      font-size: 0.8em;
+    }
+  }
+</style>
